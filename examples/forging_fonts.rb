390da44ac8235de04e62f103ff15500fab4ecbfc
Konstantin Tennhard <me@t6d.de>
2012-06-11 20:52:37 +0200

    Updates the example

--- examples/forging_fonts.rb
+++ examples/forging_fonts.rb
@@ -3,11 +3,7 @@ require 'blacksmith'
 
 Blacksmith.forge do
   
-  name "Blacksmith Medium"
   family "Blacksmith"
-  
-  source File.expand_path('../src', __FILE__)
-  target File.expand_path('../blacksmith-medium.ttf', __FILE__)
   
   glyph 'star', code: 0x0061
   

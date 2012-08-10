fd13afbfed992dd6d73913fc9fd4c5c02f857d15
Benedikt Deicke and Konstantin Tennhard <team+benedikt+t6d@rubymates.com>
2012-08-10 12:06:40 +0200

    Changes template generation to thor templates

--- lib/blacksmith/runner.rb
+++ lib/blacksmith/runner.rb
@@ -5,6 +5,8 @@ class Blacksmith::Runner < Thor
 
   default_task :generate
 
+  source_root File.expand_path('../../../support', __FILE__)
+
   desc "init", "Creates a new Blacksmith workspace"
   def init(name)
     path = name.downcase
@@ -20,7 +22,9 @@ class Blacksmith::Runner < Thor
 
   desc "generate", "Generates the font"
   def generate
-    say Time.now
+    self.font = Blacksmith::DSL.evaluate(forgefile_path)
+    template('font.css.tt', "build/#{font.basename}.css")
+    template('font.html.tt', "build/#{font.basename}.html")
   end
   
   desc "version", "Displays the version"
@@ -28,5 +32,15 @@ class Blacksmith::Runner < Thor
     say "Blacksmith v#{Blacksmith::VERSION}"
   end
 
+private
 
+  attr_accessor :font
+
+  def forgefile_path
+    File.join(Dir.pwd, 'Forgefile')
+  end
+
+  def source_paths
+    [File.join(Dir.pwd, 'support')] + super
+  end
 end
--- spec/blacksmith/runner_spec.rb
+++ spec/blacksmith/runner_spec.rb
@@ -2,9 +2,38 @@ require 'spec_helper'
 
 describe Blacksmith::Runner do
 
-
   describe '#generate' do
+    before do
+      file 'source/glyph.svg'
+      file 'Forgefile', <<-EOF
+        name 'My Font'
+  
+        glyph 'glyph.svg', :code => 0x100
+      EOF
+    end
 
+    context 'with default support files' do
+      before { capture(:stdout) { subject.generate } }
+  
+      it 'should generate a css file' do
+        File.file?('build/My-Font.css').should be_true
+      end
+  
+      it 'should generate a html file' do
+        File.file?('build/My-Font.html').should be_true
+      end
+    end
+
+    context 'with overloaded support files' do
+      before do
+        file 'support/font.css.tt', 'Custom Content'
+        capture(:stdout) { subject.generate }
+      end
+
+      it 'should use the overloaded file' do
+        File.read('build/My-Font.css').should eq("Custom Content")
+      end
+    end
   end
 
   describe '#version' do
@@ -12,11 +41,11 @@ describe Blacksmith::Runner do
 
     specify { output.should eq("Blacksmith v#{Blacksmith::VERSION}\n") }
   end
-  
+
   describe "#init" do
-    
+
     before { capture(:stdout) { subject.init('myfont') } }
-    
+
     it 'should create a build directory' do
       File.directory?('myfont/build').should be_true
     end
